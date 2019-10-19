56719d5e83ab4dd86c1d9af534b88ed1cd509d8a
Javier Roncallo <jroncallo96@gmail.com>
2019-10-19 12:02:07 -0500

    Create the fetchPost function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,32 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Time {
+  final String time;
+  final String country;
+
+  Time({this.time, this.country});
+
+  factory Time.fromJson(Map<String, dynamic> json) {
+    return Time(
+      time: json['time'],
+      country: json['country']
+    );
+  }
+}
+
+Future<Time> fetchTime() async {
+  final response =
+      await http.get('http://ec2-3-18-105-140.us-east-2.compute.amazonaws.com:3000/times');
+  if (response.statusCode == 200) {
+    return Time.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load Time');
+  }
+}
 
 void main() => runApp(MyApp());
 
