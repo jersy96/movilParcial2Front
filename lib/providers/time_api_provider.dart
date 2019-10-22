36a53a02996c39c454f3f5b56c451d6d2b25f2c5
Javier Roncallo <jroncallo96@gmail.com>
2019-10-22 17:22:20 -0500

    Add headers to the time request

--- lib/providers/time_api_provider.dart
+++ lib/providers/time_api_provider.dart
@@ -1,11 +1,18 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/time.dart';
 
 class TimeApiProvider{
   Future<Time> fetchTime() async {
-    final response =
-      await http.get('http://ec2-3-18-105-140.us-east-2.compute.amazonaws.com:3000/times');
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String token = prefs.getString('token');
+    final response = await http.get(
+      'http://ec2-3-18-105-140.us-east-2.compute.amazonaws.com:3000/times',
+      headers: {
+        "Authorization": token
+      }
+    );
     if (response.statusCode == 200) {
       return Time.fromJson(json.decode(response.body));
     } else {
