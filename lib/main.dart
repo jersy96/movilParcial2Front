8f246614c3568d65be43417cd8481da8b3244e88
Sebastian Cabarcas <sebascabarcas@gmail.com>
2019-10-21 19:42:34 -0500

    Adding design components

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:helloflutter/MyHomePage.dart';
+import 'package:helloflutter/MySecondPage.dart';
+import 'package:helloflutter/widgets/button_green.dart';
 import 'models/time.dart';
 import 'repositories/time_repository.dart';
 
@@ -12,79 +15,23 @@ class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
-
 //Text(timeString)
 class _MyAppState extends State<MyApp> {
-  Future<Time> time;
-  String timeString = '';
-  TimeRepository _timeRepository = TimeRepository();
-
-  void updateTime(){
-    time = _timeRepository.fetchTimeFromServer();
-    time.then((Time t) => setState((){
-      timeString = "${t.time} en ${t.country}";
-    }));
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Web services in flutter',
-      theme: ThemeData(
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Web services in flutter'),
-        ),
-        body: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text(timeString),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Usuario',
-                  ),
-                ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
-                ),
-                const SizedBox(height: 30),
-                RaisedButton(
-                  onPressed: () {},
-                  textColor: Colors.white,
-                  padding: const EdgeInsets.all(0.0),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: <Color>[
-                          Color(0xFF0D47A1),
-                          Color(0xFF1976D2),
-                          Color(0xFF42A5F5),
-                        ],
-                      ),
-                    ),
-                    padding: const EdgeInsets.all(10.0),
-                    child:
-                    const Text('Login', style: TextStyle(fontSize: 20)),
-                  ),
-                ),
-              ],
-            ),
-
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: updateTime,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-        ),
-      );
+      home: new MyHomePage(
+        title: 'My Home Page'
+      ),
+      routes: {
+        "/logout": (context) => MySecondPage(),
+      }
+    );
   }
+
 }
+
